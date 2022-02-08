0076700b851419fca3c5bd39b48f804b8d6ffe72
Muhammad-arafut <71282297+Muhammad-arafut@users.noreply.github.com>
2022-02-09 00:06:02 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -5,100 +5,100 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = 'HWo5n9a0MDh5XLLNjHjpvI5qgG0/bCO2wUx92DagXo1TqCiMchGXbuTP0bLwd4NslAq9bxUUn3RLvQJ585KHwHDOIIUaFMRjSh8VPBP2pEfQGO7KTsAjpPRVwvofZbvCPUUwvPSUhz9CNYUm773HigdB04t89/1O/w1cDnyilFU=';
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\FirePHPHandler;
+// use Monolog\Logger;
+// use Monolog\Handler\StreamHandler;
+// use Monolog\Handler\FirePHPHandler;
 
-$logger = new Logger('LineBot');
-$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
+// $logger = new Logger('LineBot');
+// $logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
+// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
+// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
 
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-try {
-	$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
-	error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\InvalidEventSourceException $e) {
-	error_log('parseEventRequest failed. InvalidEventSourceException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\InvalidEventSourceException $e) {
-	error_log('parseEventRequest failed. InvalidEventSourceException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
-	error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
-}
+// $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+// try {
+// 	$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+// } catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+// 	error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+// } catch(\LINE\LINEBot\Exception\InvalidEventSourceException $e) {
+// 	error_log('parseEventRequest failed. InvalidEventSourceException => '.var_export($e, true));
+// } catch(\LINE\LINEBot\Exception\InvalidEventSourceException $e) {
+// 	error_log('parseEventRequest failed. InvalidEventSourceException => '.var_export($e, true));
+// } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+// 	error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+// }
 
-foreach ($events as $event) {
+// foreach ($events as $event) {
 
-	// Postback Event
-	if (($event instanceof \LINE\LINEBot\Event\PostbackEvent)) {
-		$logger->info('Postback message has come');
-		continue;
-	}
-	// Location Event
-	if  ($event instanceof LINE\LINEBot\Event\MessageEvent\LocationMessage) {
-		$logger->info("location -> ".$event->getLatitude().",".$event->getLongitude());
-		continue;
-	}
+// 	// Postback Event
+// 	if (($event instanceof \LINE\LINEBot\Event\PostbackEvent)) {
+// 		$logger->info('Postback message has come');
+// 		continue;
+// 	}
+// 	// Location Event
+// 	if  ($event instanceof LINE\LINEBot\Event\MessageEvent\LocationMessage) {
+// 		$logger->info("location -> ".$event->getLatitude().",".$event->getLongitude());
+// 		continue;
+// 	}
 
-	// Message Event = TextMessage
-	if (($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
-		$messageText=strtolower(trim($event->getText()));
-		switch ($messageText) {
-		case "text" : 
-			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("text message");
-			break;
-		case "location" :
-			$outputText = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder("Eiffel Tower", "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France", 48.858328, 2.294750);
-			break;
-		case "button" :
-			$actions = array (
-				// general message action
-				New \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("button 1", "text 1"),
-				// URL type action
-				New \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("Google", "http://www.google.com"),
-				// The following two are interactive actions
-				New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("next page", "page=3"),
-				New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("Previous", "page=1")
-			);
-			$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
-			$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("button text", "description", $img_url, $actions);
-			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("this message to use the phone to look to the Oh", $button);
-			break;
-		case "carousel" :
-			$columns = array();
-			$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
-			for($i=0;$i<5;$i++) {
-				$actions = array(
-					new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("Add to Card","action=carousel&button=".$i),
-					new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("View","http://www.google.com")
-				);
-				$column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Title", "description", $img_url , $actions);
-				$columns[] = $column;
-			}
-			$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
-			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("Carousel Demo", $carousel);
-			break;	
-		case "image" :
-			$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
-			$outputText = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img_url, $img_url);
-			break;	
-		case "confirm" :
-			$actions = array (
-				New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("yes", "ans=y"),
-				New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("no", "ans=N")
-			);
-			$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder("problem", $actions);
-			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("this message to use the phone to look to the Oh", $button);
-			break;
-		default :
-			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("demo command: text, location, button, confirm to test message template");	
-			break;
-		}
-		$response = $bot->replyMessage($event->getReplyToken(), $outputText);
-	}
+// 	// Message Event = TextMessage
+// 	if (($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+// 		$messageText=strtolower(trim($event->getText()));
+// 		switch ($messageText) {
+// 		case "text" : 
+// 			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("text message");
+// 			break;
+// 		case "location" :
+// 			$outputText = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder("Eiffel Tower", "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France", 48.858328, 2.294750);
+// 			break;
+// 		case "button" :
+// 			$actions = array (
+// 				// general message action
+// 				New \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("button 1", "text 1"),
+// 				// URL type action
+// 				New \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("Google", "http://www.google.com"),
+// 				// The following two are interactive actions
+// 				New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("next page", "page=3"),
+// 				New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("Previous", "page=1")
+// 			);
+// 			$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
+// 			$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("button text", "description", $img_url, $actions);
+// 			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("this message to use the phone to look to the Oh", $button);
+// 			break;
+// 		case "carousel" :
+// 			$columns = array();
+// 			$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
+// 			for($i=0;$i<5;$i++) {
+// 				$actions = array(
+// 					new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("Add to Card","action=carousel&button=".$i),
+// 					new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("View","http://www.google.com")
+// 				);
+// 				$column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Title", "description", $img_url , $actions);
+// 				$columns[] = $column;
+// 			}
+// 			$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
+// 			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("Carousel Demo", $carousel);
+// 			break;	
+// 		case "image" :
+// 			$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
+// 			$outputText = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img_url, $img_url);
+// 			break;	
+// 		case "confirm" :
+// 			$actions = array (
+// 				New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("yes", "ans=y"),
+// 				New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("no", "ans=N")
+// 			);
+// 			$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder("problem", $actions);
+// 			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("this message to use the phone to look to the Oh", $button);
+// 			break;
+// 		default :
+// 			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("demo command: text, location, button, confirm to test message template");	
+// 			break;
+// 		}
+// 		$response = $bot->replyMessage($event->getReplyToken(), $outputText);
+// 	}
 
-}  
+// }  
 
 // Get POST body content
 $content = file_get_contents('php://input');
