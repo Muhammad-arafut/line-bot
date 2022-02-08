8d5d838d48787f0786a4f781ea6969338b1b97b4
Muhammad-arafut <71282297+Muhammad-arafut@users.noreply.github.com>
2022-02-09 00:28:26 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -128,9 +128,11 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text,
-				'text' => 'text',
+				'type1' => 'text1',
+				'text1' => $text,
+				'type2' => 'text2',
+				'text2' => 'text',
+				
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
