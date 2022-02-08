9cf5f4dc114b5b92e9195d3a0fd8a61431e90fae
Muhammad-arafut <71282297+Muhammad-arafut@users.noreply.github.com>
2022-02-09 00:30:51 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -129,7 +129,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "id : " . $text,
+				'text' => "user id ของท่านคือ : " . $text,
 				
 			];
 
