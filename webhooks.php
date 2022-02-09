bd05a4408d6c85c0912ea0fd71c067cdccced022
Muhammad-arafut <71282297+Muhammad-arafut@users.noreply.github.com>
2022-02-09 10:01:58 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -34,7 +34,7 @@ $access_token = 'HWo5n9a0MDh5XLLNjHjpvI5qgG0/bCO2wUx92DagXo1TqCiMchGXbuTP0bLwd4N
 					}else{
 						$messages = [
 						'type' => 'text',
-						'text' => "สวัสดี หากท่านต้องการ User id พิมพ์ Yes" "
+						'text' => "สวัสดี หากท่านต้องการ User id พิมพ์ Yes"
 					}
 					// Make a POST Request to Messaging API to reply to sender
 					$url = 'https://api.line.me/v2/bot/message/reply';
