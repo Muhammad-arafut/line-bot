656040f88ccc7578a8c7883fcdb6b4eebdb1119f
Muhammad-arafut <71282297+Muhammad-arafut@users.noreply.github.com>
2022-02-09 08:38:41 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,8 @@ $access_token = 'HWo5n9a0MDh5XLLNjHjpvI5qgG0/bCO2wUx92DagXo1TqCiMchGXbuTP0bLwd4N
 				// Reply only when message sent is in 'text' format
 				if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 					// Get text sent
-					$text = $event['source']['userId'];
+					$text = $event['message']['text'];
+// 					$text = $event['source']['userId'];
 					// Get replyToken
 					$replyToken = $event['replyToken'];
 
